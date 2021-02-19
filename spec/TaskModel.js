4e635b85270e8761069366d8dc138051d853caa7
Seth Bashford <seth.bashford@boystown.org>
2021-02-19 10:57:38 -0600

    add test

--- spec/TaskModel.js
+++ spec/TaskModel.js
@@ -1,6 +1,10 @@
 import { TaskModel, Choice } from "../lib/TaskModel.js";
 
 class AudioPlayerStub {
+  constructor() {
+    this.feedbackPlayed_ = false;
+  }
+
   feedbackPlayed() {
     return this.feedbackPlayed_;
   }
@@ -8,13 +12,29 @@ class AudioPlayerStub {
   playFeedback() {
     this.feedbackPlayed_ = true;
   }
+
+  endStimulusPlayback() {
+    this.observer.notifyThatStimulusPlaybackHasEnded();
+  }
+
+  attach(observer) {
+    this.observer = observer;
+  }
 }
 
 describe("TaskModel", () => {
-  it("should play feedback after choice is submitted", function () {
+  it("should play feedback after choice is submitted and stimulus has finished", function () {
+    const audioPlayer = new AudioPlayerStub();
+    const model = new TaskModel(audioPlayer);
+    audioPlayer.endStimulusPlayback();
+    model.submit({ choice: Choice.first });
+    expect(audioPlayer.feedbackPlayed()).toBeTrue();
+  });
+
+  it("should not play feedback after choice is submitted when stimulus has not finished", function () {
     const audioPlayer = new AudioPlayerStub();
     const model = new TaskModel(audioPlayer);
     model.submit({ choice: Choice.first });
-    expect(audioPlayer.feedbackPlayed()).toBeTrue();
+    expect(audioPlayer.feedbackPlayed()).toBeFalse();
   });
 });
