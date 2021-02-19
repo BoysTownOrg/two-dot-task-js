5a27182c7c96abc97bbd63d0eb76afabd3926ae2
Seth Bashford <seth.bashford@boystown.org>
2021-02-19 15:44:07 -0600

    add test

--- spec/TaskPresenter.js
+++ spec/TaskPresenter.js
@@ -3,22 +3,38 @@ import { TaskPresenter } from "../lib/TaskPresenter.js";
 class TaskViewStub {
   constructor() {
     this.firstDotColoredRed_ = false;
+    this.firstDotColoredBlack_ = false;
   }
 
   firstDotColoredRed() {
     return this.firstDotColoredRed_;
   }
 
+  firstDotColoredBlack() {
+    return this.firstDotColoredBlack_;
+  }
+
   colorFirstDotRed() {
     this.firstDotColoredRed_ = true;
+  }
+
+  colorFirstDotBlack() {
+    this.firstDotColoredBlack_ = true;
   }
 }
 
 describe("TaskPresenter", () => {
-  it("should color first dot red with first choice starts playing", function () {
+  it("should color first dot red when first choice starts playing", function () {
     const view = new TaskViewStub();
     const presenter = new TaskPresenter(view);
     presenter.notifyThatFirstChoiceHasStartedPlaying();
     expect(view.firstDotColoredRed()).toBeTrue();
+  });
+
+  it("should color first dot black when first choice stops playing", function () {
+    const view = new TaskViewStub();
+    const presenter = new TaskPresenter(view);
+    presenter.notifyThatFirstChoiceHasStoppedPlaying();
+    expect(view.firstDotColoredBlack()).toBeTrue();
   });
 });
