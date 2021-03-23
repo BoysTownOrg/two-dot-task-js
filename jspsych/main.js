5264a0fe72a1b978c0fc1b0a671dd22e4e87e7d4
Seth Bashford <seth.bashford@boystown.org>
2021-03-23 09:54:47 -0500

    media preloading

--- jspsych/main.js
+++ jspsych/main.js
@@ -1,95 +1,162 @@
-import { plugin } from "./plugin.js";
+import { plugin as twoDotPlugin } from "./plugin.js";
 import * as utility from "./utility.js";
-
-function startImageAudioButtonResponseTrial(display_element, trial) {
-  utility.clear(display_element);
-  const grid = utility.gridElement(2, 1);
-  utility.adopt(display_element, grid);
-  const image = new Image();
-  image.src = trial.imageUrl;
-  image.onload = () => {
-    image.width = trial.imageWidth;
-    image.height =
-      (image.naturalHeight * trial.imageWidth) / image.naturalWidth;
-  };
-  image.style.gridRow = 1;
-  image.style.gridColumn = 1;
-  utility.adopt(grid, image);
-  const buttonContainer = utility.buttonContainerElement();
-  utility.adopt(grid, buttonContainer);
-  buttonContainer.style.gridRow = 2;
-  buttonContainer.style.gridColumn = 1;
-  const button = utility.buttonElement();
-  button.textContent = "Continue";
-  button.style.visibility = "hidden";
-  utility.adopt(buttonContainer, button);
-  utility.addClickEventListener(button, () => jsPsych.finishTrial());
-  const stimulusPlayer = utility.audioPlayer(trial.stimulusUrl);
-  stimulusPlayer.onended = () => {
-    button.style.visibility = "visible";
-  };
-  stimulusPlayer.play();
-}
 
 function main() {
   const twoDotPluginId = "two-dot";
-  jsPsych.plugins[twoDotPluginId] = plugin();
+  jsPsych.plugins[twoDotPluginId] = twoDotPlugin();
 
-  jsPsych.plugins["image-audio-button-response"] = {
-    trial(display_element, trial) {
-      startImageAudioButtonResponseTrial(display_element, trial);
-    },
-    info: {
-      parameters: {},
-    },
-  };
+  jsPsych.plugins["image-audio-button-response"] = (() => {
+    jsPsych.pluginAPI.registerPreload(
+      "image-audio-button-response",
+      "stimulusUrl",
+      "audio"
+    );
+    jsPsych.pluginAPI.registerPreload(
+      "image-audio-button-response",
+      "imageUrl",
+      "image"
+    );
 
-  jsPsych.plugins["image-audio-with-feedback-button-response"] = {
-    trial(display_element, trial) {
-      utility.clear(display_element);
-      const grid = utility.gridElement(3, 1);
-      utility.adopt(display_element, grid);
-      const image = new Image();
-      image.src = trial.imageUrl;
-      image.style.gridRow = 1;
-      image.style.gridColumn = 1;
-      utility.adopt(grid, image);
-      const continueButtonContainer = utility.buttonContainerElement();
-      utility.adopt(grid, continueButtonContainer);
-      continueButtonContainer.style.gridRow = 3;
-      continueButtonContainer.style.gridColumn = 1;
-      const feedbackButtonContainer = utility.buttonContainerElement();
-      utility.adopt(grid, feedbackButtonContainer);
-      feedbackButtonContainer.style.gridRow = 2;
-      feedbackButtonContainer.style.gridColumn = 1;
-      const continueButton = utility.buttonElement();
-      continueButton.textContent = "Continue";
-      continueButton.style.visibility = "hidden";
-      utility.adopt(continueButtonContainer, continueButton);
-      utility.addClickEventListener(continueButton, () =>
-        jsPsych.finishTrial()
-      );
-      const player = utility.audioPlayer(trial.stimulusUrl);
-      player.play();
-      const feedbackPlayer = utility.audioPlayer(trial.feedbackUrl);
-      const feedbackButton = utility.buttonElement();
-      feedbackButton.textContent = "Feedback";
-      feedbackButton.style.visibility = "hidden";
-      utility.adopt(feedbackButtonContainer, feedbackButton);
-      continueButton.style.gridRow = 1;
-      continueButton.style.gridColumn = 1;
-      utility.addClickEventListener(feedbackButton, () =>
-        feedbackPlayer.play()
-      );
-      player.onended = () => {
-        continueButton.style.visibility = "visible";
-        feedbackButton.style.visibility = "visible";
-      };
-    },
-    info: {
-      parameters: {},
-    },
-  };
+    return {
+      info: {
+        parameters: {
+          stimulusUrl: {
+            type: jsPsych.plugins.parameterType.AUDIO,
+            pretty_name: "Stimulus URL",
+            default: "",
+            description: "The stimulus audio",
+          },
+          imageUrl: {
+            type: jsPsych.plugins.parameterType.IMAGE,
+            pretty_name: "Image URL",
+            default: "",
+            description: "The image",
+          },
+          imageWidth: {
+            type: jsPsych.plugins.parameterType.INT,
+            pretty_name: "Image width",
+            default: null,
+            description: "The image width in pixels",
+          },
+        },
+      },
+      trial(display_element, trial) {
+        utility.clear(display_element);
+        const grid = utility.gridElement(2, 1);
+        utility.adopt(display_element, grid);
+        const image = new Image();
+        image.src = trial.imageUrl;
+        image.onload = () => {
+          image.width = trial.imageWidth;
+          image.height =
+            (image.naturalHeight * trial.imageWidth) / image.naturalWidth;
+        };
+        image.style.gridRow = 1;
+        image.style.gridColumn = 1;
+        utility.adopt(grid, image);
+        const buttonContainer = utility.buttonContainerElement();
+        utility.adopt(grid, buttonContainer);
+        buttonContainer.style.gridRow = 2;
+        buttonContainer.style.gridColumn = 1;
+        const button = utility.buttonElement();
+        button.textContent = "Continue";
+        button.style.visibility = "hidden";
+        utility.adopt(buttonContainer, button);
+        utility.addClickEventListener(button, () => jsPsych.finishTrial());
+        const stimulusPlayer = utility.audioPlayer(trial.stimulusUrl);
+        stimulusPlayer.onended = () => {
+          button.style.visibility = "visible";
+        };
+        stimulusPlayer.play();
+      },
+    };
+  })();
+
+  jsPsych.plugins["image-audio-with-feedback-button-response"] = (() => {
+    jsPsych.pluginAPI.registerPreload(
+      "image-audio-with-feedback-button-response",
+      "stimulusUrl",
+      "audio"
+    );
+    jsPsych.pluginAPI.registerPreload(
+      "image-audio-with-feedback-button-response",
+      "feedbackUrl",
+      "audio"
+    );
+    jsPsych.pluginAPI.registerPreload(
+      "image-audio-with-feedback-button-response",
+      "imageUrl",
+      "image"
+    );
+
+    return {
+      info: {
+        name: "image-audio-with-feedback-button-response",
+        description: "",
+        parameters: {
+          stimulusUrl: {
+            type: jsPsych.plugins.parameterType.AUDIO,
+            pretty_name: "Stimulus URL",
+            default: "",
+            description: "The stimulus audio",
+          },
+          feedbackUrl: {
+            type: jsPsych.plugins.parameterType.AUDIO,
+            pretty_name: "Feedback URL",
+            default: "",
+            description: "The feedback audio",
+          },
+          imageUrl: {
+            type: jsPsych.plugins.parameterType.IMAGE,
+            pretty_name: "Image URL",
+            default: "",
+            description: "The image",
+          },
+        },
+      },
+      trial(display_element, trial) {
+        utility.clear(display_element);
+        const grid = utility.gridElement(3, 1);
+        utility.adopt(display_element, grid);
+        const image = new Image();
+        image.src = trial.imageUrl;
+        image.style.gridRow = 1;
+        image.style.gridColumn = 1;
+        utility.adopt(grid, image);
+        const continueButtonContainer = utility.buttonContainerElement();
+        utility.adopt(grid, continueButtonContainer);
+        continueButtonContainer.style.gridRow = 3;
+        continueButtonContainer.style.gridColumn = 1;
+        const feedbackButtonContainer = utility.buttonContainerElement();
+        utility.adopt(grid, feedbackButtonContainer);
+        feedbackButtonContainer.style.gridRow = 2;
+        feedbackButtonContainer.style.gridColumn = 1;
+        const continueButton = utility.buttonElement();
+        continueButton.textContent = "Continue";
+        continueButton.style.visibility = "hidden";
+        utility.adopt(continueButtonContainer, continueButton);
+        utility.addClickEventListener(continueButton, () =>
+          jsPsych.finishTrial()
+        );
+        const player = utility.audioPlayer(trial.stimulusUrl);
+        player.play();
+        const feedbackPlayer = utility.audioPlayer(trial.feedbackUrl);
+        const feedbackButton = utility.buttonElement();
+        feedbackButton.textContent = "Feedback";
+        feedbackButton.style.visibility = "hidden";
+        utility.adopt(feedbackButtonContainer, feedbackButton);
+        continueButton.style.gridRow = 1;
+        continueButton.style.gridColumn = 1;
+        utility.addClickEventListener(feedbackButton, () =>
+          feedbackPlayer.play()
+        );
+        player.onended = () => {
+          continueButton.style.visibility = "visible";
+          feedbackButton.style.visibility = "visible";
+        };
+      },
+    };
+  })();
 
   const page = document.createElement("div");
   const condition = document.createElement("div");
@@ -113,6 +180,10 @@ function main() {
     document.body.removeChild(page);
     jsPsych.init({
       timeline: [
+        {
+          type: "preload",
+          auto_preload: true,
+        },
         {
           type: "image-button-response",
           stimulus: "cat.png",
