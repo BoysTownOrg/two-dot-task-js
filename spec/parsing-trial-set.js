5a81eed49494f47f36c31d3e1cb986d669441869
Seth Bashford <seth.bashford@protonmail.com>
2022-04-06 15:50:42 -0500

    add to test

--- spec/parsing-trial-set.js
+++ spec/parsing-trial-set.js
@@ -121,5 +121,6 @@ Cued Recall Test,Novel,To,48,CuedRecall_TO_Final.wav,Topin,Topin.png,
     expect(trialSet[18].imageFileName).toBe("Baby.png");
     expect(trialSet[18].audioFileName).toBe("CuedRecall_BAY_Final.wav");
     expect(trialSet[19].type).toBe(TrialType.gameTransition);
+    expect(trialSet[20].type).toBe(TrialType.break);
   });
 });
