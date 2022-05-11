a11bcdae1bbb3b92d7c5c891b424b58e451cad3c
Seth Bashford <seth.bashford@protonmail.com>
2022-05-11 12:10:21 -0500

    refactor

---
+++ jspsych/jatos/word-learning-in-masks-main.js
@@ -0,0 +1,8 @@
+import { selectConditionBeforeRunning } from "../word-learning-in-masks.js";
+
+jatos.onLoad(() => {
+  const jsPsych = initJsPsych({
+    on_finish: () => jatos.endStudy(jsPsych.data.get().json()),
+  });
+  selectConditionBeforeRunning(jsPsych);
+});
