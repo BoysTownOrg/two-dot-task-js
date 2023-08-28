3b16d23003be9c16686416a4454dd7fa8b849184
Seth Bashford <seth.bashford@boystown.org>
2023-08-28 16:06:25 -0500

    tells me what it's doing; check glob first

--- update-assets.sh
+++ update-assets.sh
@@ -2,8 +2,17 @@
 
 set -eu
 
-rm assets/audio/*
-find ../WLN_Day 1_Experiment 1b_Seth/Audio Files/ -name "*.wav" -a -not -path "*OLD*" -exec cp {} assets/audio/ \;
-for file in assets/audio/*.wav.wav; do mv "$file" "$(dirname "$file")/$(basename "$file" .wav)"; done
-find ../WLN_Day 1_Experiment 1b_Seth/WLN_Exp 1b_Images/ -name "*.png" -a -not -path "*OLD*" -exec cp {} assets/images/ \;
-cp ../WLN_Day 1_Experiment 1b_Seth/*.xlsx assets/order.xlsx
+sourceDir="../WLN_Day 1_Experiment 1b_Seth"
+destDir=assets
+if compgen -G $destDir/audio/* > /dev/null; then
+    echo "removing old audio"
+    rm $destDir/audio/*
+fi
+echo "copying new audio"
+find "$sourceDir/Audio Files/" -name "*.wav" -a -not -path "*OLD*" -exec cp {} $destDir/audio/ \;
+echo "fixing audio extensions"
+for file in "$destDir"/audio/*.wav.wav; do mv "$file" "$(dirname "$file")/$(basename "$file" .wav)"; done
+echo "copying new images"
+find "$sourceDir/WLN_Exp 1b_Images/" -name "*.png" -a -not -path "*OLD*" -exec cp {} $destDir/images/ \;
+echo "copying new spreadsheet"
+cp "$sourceDir"/*.xlsx assets/order.xlsx
